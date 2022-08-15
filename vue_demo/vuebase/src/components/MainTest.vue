509996fe17b820cbf052739c18239c810e2c31bd
aibo chang <aibochang@163.com>
2022-08-15 23:35:11 +0800

    过滤器

---
+++ vue_demo/vuebase/src/components/MainTest.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <span>{{ number | multiply() }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainTest",
+  filters: {
+    multiply: function (val, time = 2) {
+      if (!val) return "";
+      return val * parseInt(time);
+    },
+  },
+  data() {
+    return {
+      number: 99,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- vue_demo/vuebase/src/views/AboutView.vue
+++ vue_demo/vuebase/src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <main-test />
   </div>
 </template>
+
+<script>
+import MainTest from "@/components/MainTest.vue";
+export default {
+  components: { MainTest },
+  name: "AboutView",
+};
+</script>
